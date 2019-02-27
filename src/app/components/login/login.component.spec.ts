090fd3162b112450775b370eda7bfe750b757c90
purvaja gopalakrishnan <purvajag@CESCHN.COM>
2019-02-27 16:59:37 +0530

    Created UI for login page with test

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,9 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
-import { FormBuilder, ReactiveFormsModule, FormsModule, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LoginService } from 'src/app/services/login.service';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
@@ -30,7 +30,7 @@ describe('LoginComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-  
+
   it('should return observable of boolean', () => {
     LoginSpy.and.returnValue(of(true));
     service.login('test', 'test').subscribe(
